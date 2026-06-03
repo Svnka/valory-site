77c388f55b11ad44b8c4e07eeecdc8a0c132c425
Svnka <mohamedazarkan720@gmail.com>
2026-06-03 23:31:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const ordersFile = path.join(dataDir, 'orders.json');
 
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
